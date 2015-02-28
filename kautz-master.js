93b9f5d8a01a2c6695e04bd96af566ec0855be94
Frans Ojala <frans.ojala@cs.helsinki.fi>
2015-02-28 17:56:23 +0200

    added a lot of documentation on the functions. Now attempting the resubmission of a node after a timeout on the ssh handshake.

--- kautz-master.js
+++ kautz-master.js
@@ -8,10 +8,10 @@ var fs = require('fs')
 var exec = require('ssh-exec')
 
 if (process.argv.length < 6) {
-	// console.log("Usage: node kautz-master.js <ip> <base port> <degree> <k-length>")
 	console.log("Usage: node kautz-master.js <base port> <degree> <k-length> <host1> ... <hostN>")
 	process.exit(1)
 }
+
 
 // this version will make the entire simulation into one host starting from the given port
 // one could hardcode these, but this option is given to provide a little more flexibility
@@ -24,23 +24,23 @@ var degree = parseInt(process.argv[3])
 var k_length = parseInt(process.argv[4])
 var hosts = process.argv.slice(5, process.argv.length)
 
+
 // change these according to your preferences but remember to change the original base as well
 var hash_algo = 'sha256'
 var digest_type = 'hex'
 var origin_base = 16
 
-// have some other mechanism than stdio for logging on the nodes
-// var out = fs.openSync('~/kautz-graph-overlay/out.log', 'a')
-// var err = fs.openSync('~/kautz-graph-overlay/err.log', 'a')
 
 // this version uses the generation of the entire kautz-space and assignment thereof
 // the implementation is not valid for arbitrary k-length kautz-strings (identifiers)
 var identifiers = kautz_generator(degree, k_length, hash_algo, digest_type, origin_base)
 identifiers.sort()
 
+
 // initialise the array for in-neighbours ( used in generating the proper routes for nodes )
 var in_neighbours = []
 for (var i = 0; i < identifiers.length; i++) in_neighbours[i] = 0
+
 
 // generate neighbours, i.e. routes for nodes
 var out_1 = []
@@ -51,9 +51,13 @@ for (var i=0; i < identifiers.length; i++ ) {
 	out_2[i] = index[2]
 }
 
+
 // get required ports, and execute the slave nodes
 var port_incrementor = 0
 var ports = []
+var timeouts = []
+var timedoutconnections = 0
+
 
 // do it for all the identifiers at this point
 for (var i = 0;i < identifiers.length; i++) {
@@ -61,6 +65,7 @@ for (var i = 0;i < identifiers.length; i++) {
 	getPort(host, base_port+port_incrementor, saveAddress)
 	port_incrementor++
 }
+
 
 // try to establish a connection to a port. if no connection was made
 // the callback is called with the ECONNRESUFED error and the port
@@ -75,6 +80,7 @@ function getPort(host, port, callback){
 		callback(err, address)
 	})
 }
+
 
 // callback to call on finding out something about the port. On a
 // ECONNREFUSED error mark the port as available, else try again
@@ -100,7 +106,12 @@ function assignIdentifiers() {
 	}
 }
 
+
 // return a function that does the actual starting of the kautz-node process.
+// the function will execute the node process with the kautz-node.js and related
+// parameters over ssh. For this to work properly the account must have a priv/pub
+// keypair active, and the private key must be unlocked (i.e. no user intervention
+// is tolerated when issuin ssh execution)
 function get_starter_function(i) {
 	return function() {
 		var ownS = JSON.stringify(ports[i])
@@ -113,25 +124,33 @@ function get_starter_function(i) {
 		var params = "'"+JSON.stringify(own)+"'"
 		var username = process.env.USER
 
-		// console.log(params)
-
 		exec('node ~/kautz-graph-overlay/kautz-node.js '+params, username+'@'+own.host)
 			.on('error', function(err){
-				console.log(err)
+				if ( err.code == 'ETIMEDOUT' && timeouts[i] < 5 ) {
+					timeouts[i]++
+					setTimeout(get_starter_function(i), i*10)
+				} else if ( timeouts[i] == 5 ) {
+					timedoutconnections++;
+					ports[i].TIMEDOUT = true
+					console.log("Too many timeouts on: "+params )
+				} else {
+					console.log(err)
+				}
+
 			}).pipe(process.stdout)
 
-	 	// var child = spawn('node', ['kautz-node.js', params], {
-	  //  		detached: true,
-		 //    stdio: [ 'ignore', out, err ]
-	 	// })
-	 	// child.unref()
-
-	 	if (i == identifiers.length-1) console.log("Network is up: \n")
-	}
+	 	}
 }
 
 
+// checks if we can tell something about the state of the network.
+function check_network(i) {
+	if (i < identifiers.length-1) return
 
+	console.log("Network is up: ", ports)
+	if (timedoutconnections>0) console.log("All nodes could not be initialised! Check the host listing
+		for missing clients, you may be able to set them up manually. Count: "+timedoutconnections)
+}
 
 
 
--- kautz-node.js
+++ kautz-node.js
@@ -3,14 +3,13 @@ var fs = require('fs')
 var outFile = fs.openSync('./out.log', 'a')
 var errFile = fs.openSync('./err.log', 'a')
 
-// console.log(process.argv)
-// process.exit()
-
 if (process.argv.length < 3) {
 	logger("Not enought parameters given.")
 	process.exit()
 }
 
+
+// parse the configuration from the process arguments if possible
 try {
 	var config = JSON.parse(process.argv[2])
 } catch (e) {
@@ -18,6 +17,9 @@ try {
 	process.exit()
 }
 
+
+// check that we got a proper configuration, that no important pieces are missing
+// does not actually check the validity of the fields.
 if (!config || !config.id || !config.host || !config.port || !config.out1 || !config.out2){
 	logger("Incorrect call: "+process.argv[2])
 	process.exit()
@@ -40,6 +42,9 @@ var out_2_port = config.out2.port
 
 var exit_status = 0
 
+
+// create the listening server to accept incoming connections from other nodes
+// try to protect against other processes communicating with the node.
 var server = net.createServer(function(connection) {
 	connection.on('data', function(data_in) {
 		if (data_in.toString('ascii', 0, data_in.length-2) == 'STOP') sendExit()
@@ -52,11 +57,16 @@ var server = net.createServer(function(connection) {
 		}
 	})
 })
+
+
+// at this point just throw an error, this is not a very robust server.
 server.on('error', function(err) {
 	throw err
 })
 server.listen(own_port)
 
+
+// react to some incoming data. mainly provide the actual routing for the message.
 function react(data) {
 	var destination = data.destination
 	var port
@@ -94,16 +104,23 @@ function react(data) {
 	sendMsg(data, host, port)
 }
 
+
+// grab the postfix that is associated with the particular string.
 function postfix(string, k) {
 	if (!string) return null
 	return string.slice(k, string.length)
 }
 
+
+// check if the given postfix string is actually a prefix of the node
+// routing to.
 function isPrefix(X, V) {
 	var prefix = V.slice(0, X.length)
 	return prefix == X
 }
 
+
+// Send some data over to the next node that was chosen by the routing algorithm
 function sendMsg(data, host, port) {
 	var client = net.connect({host:host, port:port}, function() {
 		client.write(JSON.stringify(data))
@@ -113,6 +130,10 @@ function sendMsg(data, host, port) {
 	})
 }
 
+
+// This tries to provide an interface to more easily enable the shutdown
+// of the entire network. It's based on the Hamiltonian path that is formed
+// inside the kautz-graph. The formality of this algorithm should be checked!
 function sendExit() {
 	var client1 = net.connect({host:out_1_host, port:out_1_port}, function() {
 		client.write('End')
@@ -132,6 +153,8 @@ function sendExit() {
 	})
 }
 
+
+// if we are able to quit after sending the exit signal to the next node.
 function end() {
 	if (exit_status==2) {
 		server.close()
@@ -139,6 +162,9 @@ function end() {
 	}
 }
 
+
+// a function to provide logging to an outise file, as no commangin terminal
+// will be attached to this process in the listening mode.
 function logger(err, msg) {
 	if (err) fs.write(errFile, err+'\n')
 	else fs.write(outFile, msg+'\n')
