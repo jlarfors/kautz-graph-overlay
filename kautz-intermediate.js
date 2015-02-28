d152154cc54e38a25eb15c76e81b9c001747da40
Frans Ojala <frans.ojala@cs.helsinki.fi>
2015-02-28 19:02:38 +0200

    the intermediate did not find the log file which is weird

--- kautz-intermediate.js
+++ kautz-intermediate.js
@@ -3,8 +3,8 @@ if (process.argv.length < 4) console.log("Not enough parameters given.")
 
 var fs = require('fs'),
      spawn = require('child_process').spawn,
-     out = fs.openSync('./logs/out.log'+process.argv[2], 'a'),
-     err = fs.openSync('./logs/err.log'+process.argv[2], 'a');
+     out = fs.openSync('./logs/'+process.argv[2], 'a'),
+     err = fs.openSync('./logs/'+process.argv[2], 'a');
 
 var child = spawn('node', ['kautz-graph-overlay/kautz-node.js', process.argv[3]], {
 	detached: true,
